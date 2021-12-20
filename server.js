8faa0b6cb0025aaa81b472e543c4efd104cf368a
Paul Mcdevitt <88408418+Orbit001@users.noreply.github.com>
2021-12-20 16:05:17 -0500

    please work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port;
 
 
 
